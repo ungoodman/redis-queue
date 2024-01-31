f8bd03691280ad38301e666d14522a5b41a4d2e5
ungoodman <suchanej.19@gmail.com>
2024-01-31 22:07:07 +0700

    add log to rest client

--- image-processing/src/main/java/com/github/ungoodman/redis/queue/image/processing/client/ReportRestTemplateClient.java
+++ image-processing/src/main/java/com/github/ungoodman/redis/queue/image/processing/client/ReportRestTemplateClient.java
@@ -1,9 +1,9 @@
 package com.github.ungoodman.redis.queue.image.processing.client;
 
 import com.github.ungoodman.redis.queue.image.processing.model.UpdateReportStatusRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class ReportRestTemplateClient {
     @Value("${backend.update-report-status-url}")
     private String updateReportStatusUrl;
@@ -25,8 +26,12 @@ public class ReportRestTemplateClient {
     }
 
     public void updateReportStatus(UpdateReportStatusRequest request) {
+        log.info("Sending Report Status to URL: {} with Data: {}", updateReportStatusUrl, request);
+
         ResponseEntity<Object> response = restTemplate.exchange(updateReportStatusUrl,
                 HttpMethod.POST, new HttpEntity<>(request), Object.class);
+
+        log.info("Response from Report Status URL: {} with Data: {}", updateReportStatusUrl, response);
 
         if (!HttpStatus.ACCEPTED.equals(response.getStatusCode()))
             throw new RestClientException(response.getStatusCode().toString());
